2e6ae49bf31b240e01a14b10f19c4307d4eb2a8b
Squabbit <bennjmann.red@gmail.com>
2024-03-06 14:15:12 +1000

    Update AuthenticationService.java

--- src/AuthenticationService.java
+++ src/AuthenticationService.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class AuthenticationService implements IAuthenticationService {
     private ArrayList<User> users;
@@ -10,17 +11,25 @@ public class AuthenticationService implements IAuthenticationService {
 
     @Override
     public User signUp(String username, String password) {
-        return null;
+        // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user;
+        //  returns null otherwise
+       for (User user : users) {
+           if (Objects.equals(user.getUsername(), username)) return null;
+       }
+       User user = new User(username, password);
+       users.add(user);
+       return user;
     }
 
     @Override
     public User logIn(String username, String password) {
+        // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
+
+        for (User login : users) {
+            if (Objects.equals(login.getPassword(), password) && Objects.equals(login.getUsername(), username)) {
+                return login;
+            }
+        }
         return null;
     }
-
-    // TODO Now: Add a constructor to initialize the users list with the default user
-
-    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
-
-    // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
 }
