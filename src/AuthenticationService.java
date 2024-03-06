5209dd432cda3f2ca297876e85d9f3d64ed7ad32
Squabbit <bennjmann.red@gmail.com>
2024-03-06 13:51:40 +1000

    added AuthenticationService file

---
+++ src/AuthenticationService.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class AuthenticationService implements IAuthenticationService {
+    private ArrayList<User> users;
+
+
+    public AuthenticationService(ArrayList<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public User signUp(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public User logIn(String username, String password) {
+        return null;
+    }
+
+    // TODO Now: Add a constructor to initialize the users list with the default user
+
+    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
+
+    // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
+}
